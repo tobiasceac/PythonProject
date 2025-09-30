ef562d15c9436d03f6b2e62ee8c99f693487c49a
tobias <tobiasceniceros@gmail.com>
2025-09-30 10:58:37 +0200

    Actualizacion

--- UT2/Ejercicio5.py
+++ UT2/Ejercicio5.py
@@ -1,1 +1,20 @@
-opcion = input("Ingrese la opcion: ")
+
+print("Menu")
+print("1. Perfil")
+print("2. Editar Perfil")
+print("3. Salir")
+try:
+
+    opcion = int(input("Ingrese la opcion: "))
+
+    while opcion != 3:
+        match opcion:
+            case 1:
+                print("Perfil")
+            case 2:
+                print("Editar Perfil")
+            case _:
+                print("error")
+        opcion = int(input("Ingrese la opcion: "))
+except:
+    print("error")
