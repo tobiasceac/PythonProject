ecd8373e937afff914c1a42167175e57d88da506
tobias <tobiasceniceros@gmail.com>
2025-09-30 13:14:42 +0200

    Actualizacion

---
+++ UT2/Ejercicio6.py
@@ -0,0 +1,12 @@
+
+try:
+    num = int(input("Ingrese el numero: "))
+
+    if num % 2 == 0:
+        print("El numero es par")
+    else:
+        print("El numero es impar")
+
+except:
+    print("Error")
+
